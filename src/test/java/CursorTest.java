f164743f6109f02342d111f06503eba62058b351
Abel Quiros <beluchin@gmail.com>
2020-05-02 12:04:21 -0400

    nested aggregate - red

--- src/test/java/CursorTest.java
+++ src/test/java/CursorTest.java
@@ -12,33 +12,52 @@ import static utils.Random.randomPrimitiveType;
 
 final class CursorTest {
     @Test
-    void ofPrimitiveCannotAdvance() {
+    void _1_ofPrimitive() {
         val cursor = Cursor.of(randomPrimitiveType());
 
         assertThat(advances(cursor)).isEqualTo(0);
     }
 
     @Test
-    void ofSimpleWrapperAdvancesOnce() {
+    void _2_ofWrapper() {
         @Data
         class Wrapper {
             public int t;
         }
+
         val cursor = Cursor.of(Wrapper.class);
 
         assertThat(advances(cursor)).isEqualTo(1);
     }
 
     @Test
-    void ofSimpleAggregateAdvancesTwice() {
+    void _3_ofAggregate() {
         @Data
         class Aggregate {
             public int t;
             public char c;
         }
+
         val cursor = Cursor.of(Aggregate.class);
 
         assertThat(advances(cursor)).isEqualTo(2);
+    }
+
+    @Test
+    void _4_ofNestedAggregate() {
+        @Data
+        class Inner {
+            public int t;
+        }
+        @Data
+        class Outer {
+            public int t;
+            public Inner inner;
+        }
+
+        val cursor = Cursor.of(Outer.class);
+
+        assertThat(advances(cursor)).isEqualTo(3);
     }
 
     private static long advances(Cursor c) {
